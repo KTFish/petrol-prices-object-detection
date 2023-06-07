69129b09f2474564fa47bafcb58ed810169d6660
KTFish <90987511+KTFish@users.noreply.github.com>
2023-06-07 07:28:01 +0200

    Implement plot_results_one_fig function

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,10 @@
-import matplotlib.pyplot as plt
 import cv2
+import os
+import matplotlib.pyplot as plt
 from typing import List
 from ultralytics.yolo.engine.results import Results
 from ultralytics.yolo.engine.model import YOLO
+from math import ceil, sqrt
 
 
 def plot_results(results: List[Results], model: YOLO) -> None:
@@ -20,3 +22,36 @@ def plot_results(results: List[Results], model: YOLO) -> None:
         plt.imshow(cv2.cvtColor(res_plotted, cv2.COLOR_BGR2RGB))
         plt.axis(False)
         plt.show()
+
+
+def plot_results_one_fig(model: YOLO, dir: str) -> None:
+    """Plots detected bounding boxes on one figure for all images from the given directory.
+
+    Args:
+        model (YOLO): Model used for inference.
+        dir (str): Directory with images.
+    """
+    # Get list of image names to plot
+    images = os.listdir(dir)
+
+    # Calculate the shape of plotting figure
+    n = ceil(sqrt(len(images)))
+    fig, ax = plt.subplots(nrows=n, ncols=n, figsize=(10, 10))
+
+    # Plotting images with bounding boxes
+    for i, img in enumerate(images):
+        img_path = dir + rf"/{img}"  # Get path to the image
+        res = model(img_path)  # Get the models predictions
+        res_plotted = res[0].plot()
+
+        ax[i // n, i % n].imshow(cv2.cvtColor(res_plotted, cv2.COLOR_BGR2RGB))
+        ax[i // n, i % n].axis("off")
+
+    # Turn off the axis for remaining subplots
+    for j in range(i + 1, n * n):
+        ax[j // n, j % n].axis("off")
+
+    plt.show()
+
+
+# TODO: Function to check if every image has a label (os.listdir can be helpfull)
